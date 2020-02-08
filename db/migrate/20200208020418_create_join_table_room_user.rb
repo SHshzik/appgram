901790690cabe307c19a01148d33b07e5ecc336c
SHshzik <andrycalnicow@gmail.com>
2020-02-08 13:22:57 +0400

    fix m2m table

--- db/migrate/20200208020418_create_join_table_room_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableRoomUser < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :Rooms, :Users do |t|
-      # t.index [:room_id, :user_id]
-      # t.index [:user_id, :room_id]
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200208020418) do
-
-  create_table "Rooms_Users", id: false, force: :cascade do |t|
-    t.integer "room_id", null: false
-    t.integer "user_id", null: false
-  end
+ActiveRecord::Schema.define(version: 20200208092100) do
 
   create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
