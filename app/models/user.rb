45bf738dea4d78d4f7a5524b484f9e837926168c
Andry <andrycalnicow@gmail.com>
2020-02-09 13:13:06 +0400

    add has many for User mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :rooms
   validates :username, uniqueness: true, presence: true
+  validates :phone, uniqueness: true, presence: true
+  has_and_belongs_to_many :rooms
+  has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
 end
