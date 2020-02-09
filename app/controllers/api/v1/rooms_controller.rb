8d24f2fff1deb427d9a0830ae51a176b4f10e2e0
Andry <andrycalnicow@gmail.com>
2020-02-09 15:31:20 +0400

    finish work with model

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,15 +1,24 @@
 module Api
   module V1
     class RoomsController < ApplicationController
-      def index
-        @rooms = Room.all
-        render json: @rooms
-      end
-
-      def show
-        @room = Room.find(params[:id])
-        render json: @room
-      end
+      # before_action :current_user
+      #
+      # def index
+      #   @rooms = Room.all
+      #   render json: @rooms
+      # end
+      #
+      # def show
+      #   puts @user
+      #   @room = Room.find(params[:id])
+      #   render json: @room
+      # end
+      #
+      # private
+      #
+      # def current_user
+      #   @user ||= User.find(1)
+      # end
     end
   end
 end
