b2b37bc006d280399815fa9d353dc47fdbf2cbab
SHshzik <andrycalnicow@gmail.com>
2020-02-11 07:50:18 +0400

    rewrite seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 u = User.create(id: 1, username: Faker::Name.name, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
 
-5.times do
-  User.create(username: Faker::Name.name, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
-end
-
-r = Room.create
-r.users<<u
-
 100.times do
-  Message.create msg: 'Hello', sender_id: u.id, room: r
+  another_user = User.create(username: Faker::Name.name, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
+  room = Room.create
+  room.users << [u, another_user]
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: u, status: Faker::Boolean.boolean
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user, status: Faker::Boolean.boolean
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user, status: Faker::Boolean.boolean
 end
