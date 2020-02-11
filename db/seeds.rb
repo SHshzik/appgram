7d7a340ff93bc04ead03e98b0886faba087cea4f
SHshzik <andrycalnicow@gmail.com>
2020-02-11 12:51:29 +0400

    fix seed;

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ u = User.create(id: 1, username: Faker::Name.name, phone: Faker::PhoneNumber.pho
   another_user = User.create(username: Faker::Name.name, phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
   room = Room.create
   room.users << [u, another_user]
-  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: u, status: Faker::Boolean.boolean
-  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user, status: Faker::Boolean.boolean
-  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user, status: Faker::Boolean.boolean
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: u.id, status: Faker::Boolean.boolean
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user.id, status: Faker::Boolean.boolean
+  room.messages.create msg: Faker::Lorem.question, room: room, sender_id: another_user.id, status: Faker::Boolean.boolean
 end
